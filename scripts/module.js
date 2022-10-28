c9b585eadd69f345f004fd95f2bf2a2f9656cc14
Christian <christianzim@live.de>
2022-10-28 11:10:31 +0200

    added button and script

--- scripts/module.js
+++ scripts/module.js
@@ -1,13 +1,12 @@
 import { createLightButton } from "./lib.js"
-import { LightSourcesHandler } from "./light-sources-handler.js"
+import { startInchryptianScript } from "./light-sources-handler.js"
 
 Hooks.on('renderTokenHUD', (hud, html, title) => {
     const lightButton = createLightButton();
     const actor = game.actors.get(title.actorId)
 
     lightButton.on('click', () => {
-        let lightSourceHandler = new LightSourcesHandler()
-        lightSourceHandler.startInchryptianScript(canvas.tokens.ownedTokens.find(t => t.actor.id == title.actorId ))
+        startInchryptianScript(canvas.tokens.ownedTokens.find(t => t.actor.id == title.actorId ))
     })
 
     html.children('.left').append(lightButton)
