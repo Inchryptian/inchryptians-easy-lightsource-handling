9137cb3fa203664d978d936946ccf5ad88008a3a
Christian <christianzim@live.de>
2022-10-28 09:49:37 +0200

    added button and script

---
+++ scripts/module.js
@@ -0,0 +1,12 @@
+import {createLightButton} from "./lib.js"
+
+Hooks.on('renderTokenHUD', (hud, html, title) => {
+    const lightButton = createLightButton();
+    const actor = game.actors.get(title.actorId)
+
+    lightButton.on('click', () => {
+        start(canvas.tokens.ownedTokens.find(t => t.actor.id == title.actorId ))
+    })
+
+    html.children('.left').append(lightButton)
+})
