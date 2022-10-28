c2896e5649dce04676c9a3e24ddca8051644799a
Christian <christianzim@live.de>
2022-10-28 10:18:59 +0200

    added button and script

--- scripts/lib.js
+++ scripts/lib.js
@@ -3,8 +3,8 @@ export function createLightButton() {
 
     button.classList.add('control-icon')
     button.classList.add('artwork-open')
-    button.innerHTML = `<i class="fas fa-image fa-fw"></i>`
-    button.title = 'Show Art'
+    button.innerHTML = `<i class="fas fa-lightbulb"></i>`
+    button.title = 'Handle Light'
 
     return $(button)
 }
--- scripts/module.js
+++ scripts/module.js
@@ -1,11 +1,11 @@
-import {createLightButton} from "./lib.js"
+import { createLightButton } from "./lib.js"
 
 Hooks.on('renderTokenHUD', (hud, html, title) => {
     const lightButton = createLightButton();
     const actor = game.actors.get(title.actorId)
 
     lightButton.on('click', () => {
-        start(canvas.tokens.ownedTokens.find(t => t.actor.id == title.actorId ))
+        startinchryptianscript(canvas.tokens.ownedTokens.find(t => t.actor.id == title.actorId ))
     })
 
     html.children('.left').append(lightButton)
